a031c04950fbe804abd1db4a5de26b0aef4b1400
JoJoJet <21144246+JoJoJet@users.noreply.github.com>
2022-10-11 17:53:43 -0400

    Use better names in the example

--- examples/foo.rs
+++ examples/foo.rs
@@ -4,8 +4,8 @@ use bevy_trait_query::*;
 fn main() {
     let mut app = App::new();
     app.add_plugins(MinimalPlugins)
-        .register_component_as::<dyn Foo, Bar>()
-        .register_component_as::<dyn Foo, Gub>()
+        .register_component_as::<dyn Person, Bar>()
+        .register_component_as::<dyn Person, Gub>()
         .add_startup_system(setup)
         .add_system(print_info)
         .add_system(age_up.after(print_info))
@@ -14,18 +14,18 @@ fn main() {
     app.update();
 }
 
-pub trait Foo: 'static {
+pub trait Person: 'static {
     fn name(&self) -> &str;
     fn age(&self) -> u32;
     fn set_age(&mut self, age: u32);
 }
 
-impl_dyn_query!(Foo);
+impl_dyn_query!(Person);
 
 #[derive(Component)]
 pub struct Bar(String, u32);
 
-impl Foo for Bar {
+impl Person for Bar {
     fn name(&self) -> &str {
         &self.0
     }
@@ -40,7 +40,7 @@ impl Foo for Bar {
 #[derive(Component)]
 pub struct Gub(u32);
 
-impl Foo for Gub {
+impl Person for Gub {
     fn name(&self) -> &str {
         "reginald"
     }
@@ -58,18 +58,18 @@ fn setup(mut commands: Commands) {
     commands.spawn().insert(Gub(27));
 }
 
-fn print_info(q: Query<&dyn Foo>) {
+fn print_info(people: Query<&dyn Person>) {
     println!("All people:");
-    for foo in &q {
-        println!("{}: {}", foo.name(), foo.age());
+    for person in &people {
+        println!("{}: {}", person.name(), person.age());
     }
     println!();
 }
 
-fn age_up(mut q: Query<&mut dyn Foo>) {
-    for foo in &mut q {
-        let new_age = foo.age() + 1;
-        foo.set_age(new_age);
+fn age_up(mut people: Query<&mut dyn Person>) {
+    for person in &mut people {
+        let new_age = person.age() + 1;
+        person.set_age(new_age);
     }
 }
 
