e807148dd6d3179d2adc286487bb22c2c52781c5
JoJoJet <21144246+JoJoJet@users.noreply.github.com>
2022-10-12 19:42:04 -0400

    add benchmarks for universal queries

---
+++ benches/universal.rs
@@ -0,0 +1,126 @@
+#![allow(clippy::all)]
+
+use bevy::ecs::prelude::*;
+use bevy_trait_query::*;
+use criterion::*;
+use std::fmt::Display;
+
+/// Define a trait for our components to implement.
+pub trait Messages: 'static {
+    fn messages(&self) -> &[String];
+    fn send_message(&mut self, _: &dyn Display);
+}
+
+// Add `WorldQuery` impls for `dyn Message`
+impl_dyn_query!(Messages);
+
+#[derive(Component)]
+pub struct RecA {
+    messages: Vec<String>,
+}
+
+impl Messages for RecA {
+    fn messages(&self) -> &[String] {
+        &self.messages
+    }
+    fn send_message(&mut self, msg: &dyn Display) {
+        self.messages.push(msg.to_string());
+    }
+}
+
+#[derive(Component)]
+pub struct RecB {
+    messages: Vec<String>,
+}
+
+impl Messages for RecB {
+    fn messages(&self) -> &[String] {
+        &self.messages
+    }
+    fn send_message(&mut self, msg: &dyn Display) {
+        self.messages.push(msg.to_string());
+    }
+}
+
+pub struct Benchmark<'w>(World, QueryState<All<&'w dyn Messages>>);
+
+impl<'w> Benchmark<'w> {
+    // Each entity only has one component in practice.
+    fn one() -> Self {
+        let mut world = World::new();
+
+        world.register_component_as::<dyn Messages, RecA>();
+        world.register_component_as::<dyn Messages, RecB>();
+
+        for _ in 0..5_000 {
+            world.spawn().insert(RecA { messages: vec![] });
+        }
+        for _ in 0..5_000 {
+            world.spawn().insert(RecB { messages: vec![] });
+        }
+
+        let query = world.query::<All<&dyn Messages>>();
+        Self(world, query)
+    }
+    fn multiple() -> Self {
+        let mut world = World::new();
+
+        world.register_component_as::<dyn Messages, RecA>();
+        world.register_component_as::<dyn Messages, RecB>();
+
+        for _ in 0..10_000 {
+            world
+                .spawn()
+                .insert_bundle((RecA { messages: vec![] }, RecB { messages: vec![] }));
+        }
+
+        let query = world.query::<All<&dyn Messages>>();
+        Self(world, query)
+    }
+    // Queries with only one, and queries with mutliple.
+    pub fn distributed() -> Self {
+        let mut world = World::new();
+
+        world.register_component_as::<dyn Messages, RecA>();
+        world.register_component_as::<dyn Messages, RecB>();
+
+        for _ in 0..2_500 {
+            world.spawn().insert(RecA { messages: vec![] });
+        }
+        for _ in 0..2_500 {
+            world.spawn().insert(RecB { messages: vec![] });
+        }
+        for _ in 0..5_000 {
+            world
+                .spawn()
+                .insert_bundle((RecA { messages: vec![] }, RecB { messages: vec![] }));
+        }
+
+        let query = world.query::<All<&dyn Messages>>();
+        Self(world, query)
+    }
+
+    pub fn run(&mut self) {
+        for all in self.1.iter_mut(&mut self.0) {
+            for x in all {
+                criterion::black_box(x);
+            }
+        }
+    }
+}
+
+pub fn one(c: &mut Criterion) {
+    let mut benchmark = Benchmark::one();
+    c.bench_function("one", |b| b.iter(|| benchmark.run()));
+}
+pub fn multiple(c: &mut Criterion) {
+    let mut benchmark = Benchmark::multiple();
+    c.bench_function("multiple", |b| b.iter(|| benchmark.run()));
+}
+pub fn distributed(c: &mut Criterion) {
+    let mut benchmark = Benchmark::distributed();
+    c.bench_function("distributed", |b| b.iter(|| benchmark.run()));
+}
+
+criterion_group!(universal, one, multiple, distributed);
+criterion_main!(universal);
