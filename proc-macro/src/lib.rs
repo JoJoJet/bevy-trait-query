33907475ef744853735bd23f14680f3409165916
JoJoJet <21144246+JoJoJet@users.noreply.github.com>
2022-10-29 20:24:13 -0400

    generate impls for read-only trait objects

--- proc-macro/src/lib.rs
+++ proc-macro/src/lib.rs
@@ -21,24 +21,147 @@ fn impl_trait_query(item: TokenStream) -> TokenStream2 {
             Span::call_site(),
         )));
 
-    let trait_query = quote! { bevy_trait_query::TraitQuery };
+    let trait_object = quote! { dyn #trait_name #trait_generics };
 
-    let trait_query_marker = quote! { bevy_trait_query::TraitQueryMarker };
-    let component = quote! { bevy_trait_query::imports::Component };
+    let my_crate = quote! { bevy_trait_query };
+    let imports = quote! { #my_crate::imports };
+
+    let trait_query = quote! { #my_crate::TraitQuery };
 
     let mut marker_generics = trait_definition.generics.clone();
     marker_generics
         .params
-        .push(parse_quote!(__T: #trait_name + #component));
+        .push(parse_quote!(__T: #trait_name + #imports::Component));
     let (marker_impl_generics, ..) = marker_generics.split_for_impl();
 
-    let trait_query_marker_impl = quote! {
-        impl #marker_impl_generics #trait_query_marker::<dyn #trait_name #trait_generics> for (__T,)
+    let marker_impl_code = quote! {
+        impl #impl_generics #trait_query for #trait_object #where_clause {}
+
+        impl #marker_impl_generics #my_crate::TraitQueryMarker::<#trait_object> for (__T,)
         #where_clause
         {
             type Covered = __T;
-            fn cast(ptr: *mut u8) -> *mut dyn #trait_name #trait_generics {
+            fn cast(ptr: *mut u8) -> *mut #trait_object {
                 ptr as *mut __T as *mut _
+            }
+        }
+    };
+
+    let mut generics_with_lifetime = trait_definition.generics.clone();
+    generics_with_lifetime.params.insert(0, parse_quote!('__a));
+    let (impl_generics_with_lifetime, ..) = generics_with_lifetime.split_for_impl();
+
+    let trait_object_query_code = quote! {
+        impl #impl_generics_with_lifetime #imports::WorldQueryGats<'__a> for &#trait_object
+        #where_clause
+        {
+            type Item = #my_crate::ReadTraits<'__a, #trait_object>;
+            type Fetch = #my_crate::ReadAllTraitsFetch<'__a, #trait_object>;
+        }
+
+        unsafe impl #impl_generics #imports::ReadOnlyWorldQuery for &#trait_object {}
+
+        unsafe impl #impl_generics_with_lifetime #imports::WorldQuery for &'__a #trait_object {
+            type ReadOnly = Self;
+            type State = #my_crate::TraitQueryState<#trait_object>;
+
+            #[inline]
+            unsafe fn init_fetch<'w>(
+                world: &'w World,
+                state: &Self::State,
+                last_change_tick: u32,
+                change_tick: u32,
+            ) -> <Self as #imports::WorldQueryGats<'w>>::Fetch {
+                <#my_crate::All<&#trait_object> as #imports::WorldQuery>::init_fetch(
+                    world,
+                    state,
+                    last_change_tick,
+                    change_tick,
+                )
+            }
+
+            #[inline]
+            unsafe fn clone_fetch<'w>(
+                fetch: &<Self as #imports::WorldQueryGats<'w>>::Fetch,
+            ) -> <Self as #imports::WorldQueryGats<'w>>::Fetch {
+                <#my_crate::All<&#trait_object> as #imports::WorldQuery>::clone_fetch(fetch)
+            }
+
+            #[inline]
+            fn shrink<'wlong: 'wshort, 'wshort>(
+                item: #imports::QueryItem<'wlong, Self>,
+            ) -> #imports::QueryItem<'wshort, Self> {
+                item
+            }
+
+            const IS_DENSE: bool = <#my_crate::All<&#trait_object> as #imports::WorldQuery>::IS_DENSE;
+            const IS_ARCHETYPAL: bool =
+                <#my_crate::All<&#trait_object> as #imports::WorldQuery>::IS_ARCHETYPAL;
+
+            #[inline]
+            unsafe fn set_archetype<'w>(
+                fetch: &mut <Self as #imports::WorldQueryGats<'w>>::Fetch,
+                state: &Self::State,
+                archetype: &'w #imports::Archetype,
+                tables: &'w #imports::Table,
+            ) {
+                <#my_crate::All<&#trait_object> as #imports::WorldQuery>::set_archetype(
+                    fetch, state, archetype, tables,
+                );
+            }
+
+            #[inline]
+            unsafe fn set_table<'w>(
+                fetch: &mut <Self as #imports::WorldQueryGats<'w>>::Fetch,
+                state: &Self::State,
+                table: &'w #imports::Table,
+            ) {
+                <#my_crate::All<&#trait_object> as #imports::WorldQuery>::set_table(fetch, state, table);
+            }
+
+            #[inline]
+            unsafe fn fetch<'w>(
+                fetch: &mut <Self as #imports::WorldQueryGats<'w>>::Fetch,
+                entity: #imports::Entity,
+                table_row: usize,
+            ) -> <Self as #imports::WorldQueryGats<'w>>::Item {
+                <#my_crate::All<&#trait_object> as #imports::WorldQuery>::fetch(
+                    fetch,
+                    entity,
+                    table_row,
+                )
+            }
+
+            #[inline]
+            fn update_component_access(
+                state: &Self::State,
+                access: &mut #imports::FilteredAccess<#imports::ComponentId>,
+            ) {
+                <#my_crate::All<&#trait_object> as #imports::WorldQuery>::update_component_access(
+                    state, access,
+                );
+            }
+
+            #[inline]
+            fn update_archetype_component_access(
+                state: &Self::State,
+                archetype: &#imports::Archetype,
+                access: &mut #imports::Access<#imports::ArchetypeComponentId>,
+            ) {
+                <#my_crate::All<&#trait_object> as #imports::WorldQuery>::update_archetype_component_access(state, archetype, access);
+            }
+
+            #[inline]
+            fn init_state(world: &mut World) -> Self::State {
+                <#my_crate::All<&#trait_object> as #imports::WorldQuery>::init_state(world)
+            }
+
+            #[inline]
+            fn matches_component_set(
+                state: &Self::State,
+                set_contains_id: &impl Fn(#imports::ComponentId) -> bool,
+            ) -> bool {
+                <#my_crate::All<&#trait_object> as #imports::WorldQuery>::matches_component_set(state, set_contains_id)
             }
         }
     };
@@ -46,9 +169,9 @@ fn impl_trait_query(item: TokenStream) -> TokenStream2 {
     quote! {
         #trait_definition
 
-        impl #impl_generics #trait_query for dyn #trait_name #trait_generics #where_clause {}
+        #marker_impl_code
 
-        #trait_query_marker_impl
+        #trait_object_query_code
     }
 }
 
